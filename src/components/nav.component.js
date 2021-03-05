45de30e9e8afbd291111b2e2c3dce40d1e050f5e
Hoe Feng Ting <fengting1995@gmail.com>
2021-03-05 15:26:21 +0800

    test

--- src/components/nav.component.js
+++ src/components/nav.component.js
@@ -12,8 +12,6 @@ export default class Nav extends Component {
                     <li className="nav-item">
                         <Link className="nav-link" to={"/login"}>Login</Link>
                     </li>
-<<<<<<< HEAD
-=======
                     <li className="nav-item">
                         <Link className="nav-link" to={"/register"}>Sign up</Link>
                     </li>
@@ -23,13 +21,7 @@ export default class Nav extends Component {
                     <li className="nav-item">
                         <Link className="nav-link" to={"/UserInfo"}>User Info</Link>
                     </li>
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 2d03689f94a01f0f243c534b1964101103dbee81
-=======
->>>>>>> 96c17b6af2d99ee15e4b0c6ffc5df6162755af96
-=======
->>>>>>> c76e0df481c3fb27f761d661141925f7a17396fb
+
                     </ul>
                 </div>
                 </div>
