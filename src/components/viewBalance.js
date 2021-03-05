96c17b6af2d99ee15e4b0c6ffc5df6162755af96
desmondchuash <80082285+desmondchuash@users.noreply.github.com>
2021-03-05 15:12:13 +0800

    Update viewBalance.js

--- src/components/viewBalance.js
+++ src/components/viewBalance.js
@@ -11,10 +11,6 @@ import { Animation } from '@devexpress/dx-react-chart';
 const data = [
   { region: 'Asia', val: 4119626293 },
   { region: 'Africa', val: 1012956064 },
-  { region: 'Northern America', val: 344124520 },
-  { region: 'Latin America and the Caribbean', val: 590946440 },
-  { region: 'Europe', val: 727082222 },
-  { region: 'Oceania', val: 35104756 },
 ];
 
 export default class Demo extends React.PureComponent {
