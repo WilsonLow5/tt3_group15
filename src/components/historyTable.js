b2da6e31f9606ba61010a00c497acbd751343371
WilsonLow5 <79908252+WilsonLow5@users.noreply.github.com>
2021-03-05 13:48:36 +0800

    completed historyTable

--- src/components/historyTable.js
+++ src/components/historyTable.js
@@ -12,8 +12,6 @@ import Paper from '@material-ui/core/Paper';
 const useStyles = makeStyles({
 });
 
-
-
 export default function BasicTable() {
   const classes = useStyles();
   const [items, setItems] = useState([]);
@@ -31,9 +29,8 @@ export default function BasicTable() {
         };
         await axios.post(url, article, { headers })
             .then((response) => {
-                    setItems(response.request);
+                    setItems(response.data);
                     setLoading(false);
-                    console.log(response.request);
                     
             })
             .catch((error) => {console.log(error);})
@@ -49,9 +46,6 @@ export default function BasicTable() {
     };
   }, []);
 
-  console.log(items);
-
-
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
@@ -65,20 +59,20 @@ export default function BasicTable() {
             <TableCell align="right">Asset Price</TableCell>
             <TableCell align="right">Cash Amount</TableCell>
           </TableRow>
+         
         </TableHead>
         <TableBody>
-            {items.response.map((row) => (
+        {items.map((row) =>  (
             <TableRow key={row.transactionId}>
               <TableCell component="th" scope="row">
                 {row.transactionId}
               </TableCell>
-              {/* <TableCell align="right">{row.transID}</TableCell>
               <TableCell align="right">{row.orderType}</TableCell>
-              <TableCell align="right">{row.timeStamp}</TableCell>
+              <TableCell align="right">{row.timestamp}</TableCell>
               <TableCell align="right">{row.assetSymbol}</TableCell>
               <TableCell align="right">{row.assetAmount}</TableCell>
               <TableCell align="right">{row.assetPrice}</TableCell>
-              <TableCell align="right">{row.cashAmount}</TableCell> */}
+              <TableCell align="right">{row.cashAmount}</TableCell>
             </TableRow>
           ))}
         </TableBody>
